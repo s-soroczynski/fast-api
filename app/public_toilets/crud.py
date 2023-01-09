880670f12bc015b51a90a22a747c9fb8161be043
s-soroczynski <sebastian.soroczynski23@gmail.com>
2023-01-09 18:01:25 +0100

    refactor public_toilets

--- app/public_toilets/crud.py
+++ app/public_toilets/crud.py
@@ -18,23 +18,23 @@ def get_public_toilets(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.PublicToilet).offset(skip).limit(limit).all()
 
 
-def create_public_toilet(db: Session, public_toilet: schemas.PublicToilet, user: User):
-    db_public_toilet = models.PublicToilet(
-        name=public_toilet.name,
-        lat=public_toilet.lat,
-        lng=public_toilet.lng,
-        user=user,
-        description=public_toilet.description,
-    )
-    db.add(db_public_toilet)
-    db.commit()
-    db.refresh(db_public_toilet)
-    return db_public_toilet
-
-
 def get_user_public_toilets(db: Session, user_id: int):
     return (
         db.query(models.PublicToilet)
         .filter(models.PublicToilet.user_id == user_id)
         .all()
     )
+
+
+def create_public_toilet(db: Session, item: schemas.PublicToilet, user: User):
+    public_toilet = models.PublicToilet(
+        name=item.name,
+        lat=item.lat,
+        lng=item.lng,
+        user=user,
+        description=item.description,
+    )
+    db.add(public_toilet)
+    db.commit()
+    db.refresh(public_toilet)
+    return public_toilet
